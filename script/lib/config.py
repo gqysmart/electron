debf6c9c9bf67fdec7665db0660e3d3e3ef296b1
deepak1556 <hop2deep@gmail.com>
2017-02-09 15:06:20 +0530

    update libcc commit

--- script/lib/config.py
+++ script/lib/config.py
@@ -9,7 +9,7 @@ import sys
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = os.getenv('LIBCHROMIUMCONTENT_COMMIT') or \
-    'e0da1e9caa7c8f3da3519963a9ea32abba43c7c8'
+    'a3f204ad0b9e90770d0975bd027a7de21a60b054'
 
 PLATFORM = {
   'cygwin': 'win32',
